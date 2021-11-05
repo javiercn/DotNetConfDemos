b78e4043f80c3a1e768cd9da6646aecc4aefd9cd
Daniel Roth <daroth@microsoft.com>
2021-11-04 22:35:16 -0700

    Update visuals

---
+++ DotNetConfDemos/Data/HaveChildrenExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNetConfDemos.Data
+{
+    public static class HaveChildrenExtensions
+    {
+        public static IEnumerable<object> GetAncestry(this IHaveChildren hasChildren)
+        {
+            List<object> ancestors = new() { hasChildren };
+            while (hasChildren.Parent != null)
+            {
+                ancestors.Add(hasChildren.Parent);
+                hasChildren = (IHaveChildren)hasChildren.Parent;
+            }
+            return ancestors;
+        }
+    }
+}
