07cd8537120f3e0981700297c5d45a4676a4c09c
Daniel Roth <daroth@microsoft.com>
2021-11-22 10:44:20 -0800

    Render doubles in SVG using invariant culture

---
+++ NextGenBlazorComponents/SvgHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGenBlazorComponents
+{
+    static class SvgHelpers
+    {
+        internal static string Coord(double d) => d.ToString(CultureInfo.InvariantCulture);
+    }
+}
